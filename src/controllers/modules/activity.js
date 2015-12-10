56d617a55ee4d10098529c749ab531d4a33df88e
node-rookie <749924911@qq.com>
2015-12-10 16:26:18 +0800

    debug

--- src/controllers/modules/activity.js
+++ src/controllers/modules/activity.js
@@ -4,6 +4,13 @@ var userinfoAuthFilter = require('../../middlewares/userinfo-auth-filter');
 
 module.exports = function(router){
     require('../../app/routes-spa')(router);
+    router.get('/clear', function *(){
+        console.log('******************');
+        this.cookies.set(this.tokenKey, null, {maxAge: 1});
+        this.session.authenticated = false;
+        this.session[this.userKey] = null;
+        this.body = 'clear successful!!';
+    });
 
     router.get('/activity', baseAuthFilter, function *(){
         console.log('******************');
