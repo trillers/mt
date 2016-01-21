5991f107bf45f2c6849ef7c04be2071ddace6a96
node-rookie <749924911@qq.com>
2016-01-21 13:54:38 +0800

    upload voice

--- src/controllers/api/qn.js
+++ src/controllers/api/qn.js
@@ -17,9 +17,10 @@ module.exports = function(router){
         console.log(self.query);
         var media_id = self.query.media_id;
         try {
+            console.error(media_id);
             var voiceBuffer = yield wechatApi.getMediaAsync(media_id);
             console.log(voiceBuffer);
-            qnClient.upload(voiceBuffer, {key: 'qn/test/voice/' + media_id + '.amr'}, function(err, result){
+            qnClient.upload(voiceBuffer[0], {key: 'qn/test/voice/' + media_id + '.amr'}, function(err, result){
                 if(!err){
                     console.error(result);
                 }
